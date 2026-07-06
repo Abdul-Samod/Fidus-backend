d66d6bbb819f9892119c1fc24c378ce0c79ce4c5
Abdulsamod Olaoye <samodolaoye@gmail.com>
2026-07-07 00:55:13 +0100

    feat: KYC verification module

--- fidus-backend/src/routes/kyc.ts
+++ fidus-backend/src/routes/kyc.ts
@@ -1,5 +1,7 @@
 import { Router, type Response } from 'express';
 import { requireAuth, type AuthRequest } from '../middleware/auth.js';
+import { upload } from '../middleware/upload.js';
+import { url } from 'node:inspector';
 
 const router = Router();
 
@@ -9,6 +11,30 @@ router.get('/status', requireAuth, (req: AuthRequest, res: Response) => {
         message: 'Access Granted to KYC route',
         user: req.user
     });
+});
+
+// KYC document upload (NIN)
+router.post('/upload-nin', requireAuth, upload.single('nin_document'), 
+(req: AuthRequest, res: Response): void => {
+    // Validation: No file attached
+    if (!req.file) {
+        res.status(400).json({
+            status: "error",
+            message: "No document found. Please attach your NIN image.",
+        });
+        return;
+    }
+
+    // Cloudinary puts URL inside file path
+    const secureUrl = req.file.path;
+
+    // Send the URL back to client to confirm success
+    res.status(200).json({
+        status: "success",
+        message: "NIN successfully uploaded to the cloud!",
+        url: secureUrl,
+        user: req.user
+    }) ;
 });
 
 export default router;
